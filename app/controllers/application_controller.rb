e97e5188033a244c299d1671819419617e934239
coker2019 <olabisi2k11@gmail.com>
2023-11-03 10:34:50 +0100

    Add files

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,3 @@
+class ApplicationController < ActionController::Base
+  # Your code here
+end
---
+++ db/migrate/20231102200444_rename_user_id.rb
@@ -0,0 +1,5 @@
+class RenameUserId < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :posts, :user_id, :author_id;
+  end
+end
