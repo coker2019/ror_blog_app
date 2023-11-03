fc665718bd861fef13ed034beeb1b69dd9f9808d
coker2019 <olabisi2k11@gmail.com>
2023-11-03 12:48:21 +0100

    update changes.

--- db/migrate/20231102192651_post.rb
+++ db/migrate/20231102192651_post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :author, foreign_key: { to_table: 'users' }
       t.string :title
       t.text :text
       t.integer :comments_counter
