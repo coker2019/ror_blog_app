e4183393d0a7a0c506ea416e8da4a015199266f8
coker2019 <olabisi2k11@gmail.com>
2023-11-04 16:59:25 +0100

    setup branch.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :name, presence: true
+  validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0  }
+
   def recent_post
     posts.order('created_at Desc').limit(3)
   end
---
+++ spec/models/like_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  let(:user) { create(:user) }
+  let(:post) { create(:post, author: user) }
+
+  describe 'Associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:post) }
+  end
+
+  describe '#update_likes_counter' do
+    it 'updates the post\'s likes_counter' do
+      # rubocop:disable Style/HashSyntax
+      create(:like, post: post, user: user)
+
+      expect { create(:like, post: post, user: user) }.to change { post.reload.likes_counter }.by(1)
+      # rubocop:enable Style/HashSyntax
+    end
+  end
+end
