75609f4dfd67da4aa7cc8ff496f16e01e7333386
coker2019 <olabisi2k11@gmail.com>
2023-11-04 13:57:20 +0100

    update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,16 +3,16 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  after_save :update_post_counter
+
   # Public Methods
   def five_most_recent_comments
     comments.order(created_at: :desc).limit(5)
   end
 
-  after_save :update_post_counter
-
   private
 
   def update_post_counter
-    author.increment!(:posts_counter)
+    author.increment!(:post_counter)
   end
 end
