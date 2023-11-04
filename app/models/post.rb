2c0bf8811846cf43a7a9026603fc4834e05bf42e
coker2019 <olabisi2k11@gmail.com>
2023-11-04 10:09:44 +0100

    add model for post.

---
+++ app/models/post.rb
@@ -0,0 +1,16 @@
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :likes
+  has_many :comments
+
+  private
+
+  def update_post_counter
+    author.update(posts_counter: author.posts.count)
+  end
+
+  # Public Methods
+  def five_most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+end
