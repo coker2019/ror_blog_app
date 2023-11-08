90ca270341c8680ee5a41f57f6f4a65b7f5494ea
coker2019 <olabisi2k11@gmail.com>
2023-11-08 14:55:10 +0100

    update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,11 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes
   has_many :comments
+  
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
 
   after_save :update_post_counter
 
@@ -13,6 +18,6 @@ class Post < ApplicationRecord
   private
 
   def update_post_counter
-    author.increment!(:post_counter)
+    author.increment!(:posts_counter)
   end
 end
