8d3926ba12369db949f3425d0dc46ab9ba7b1c03
coker2019 <olabisi2k11@gmail.com>
2023-11-12 17:28:19 +0100

    fix linters.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
 class Comment < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
-  
-    after_save :update_post_comments_counter
-  
-    def update_post_comments_counter
-      post.increment!(:comments_counter)
-    end
+  belongs_to :user
+  belongs_to :post
+
+  after_save :update_post_comments_counter
+
+  def update_post_comments_counter
+    post.increment!(:comments_counter)
   end
+end
