dc3b2cc841128ade47f7281b81da36754988262e
coker2019 <olabisi2k11@gmail.com>
2023-11-14 12:29:28 +0100

    Add test files

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'returns a success response' do
+      user = User.create!(name: 'Name', posts_counter: 0)
+      get user_posts_path(user_id: user.id)
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include('All Posts by a Given User')
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET /show' do
+    it 'returns a success response' do
+      user = User.create!(name: 'Name', posts_counter: 0)
+      post = Post.create!(title: 'Post Title', author_id: user.id, comments_counter: 0, likes_counter: 0)
+      get user_post_path(user_id: user.id, id: post.id)
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include('Single Post')
+      expect(response).to render_template(:show)
+    end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,21 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'returns a success response' do
+      get users_path
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include('All Users')
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET /show' do
+    it 'returns a success response' do
+      user = User.create!(name: 'Name', posts_counter: 0)
+      get user_path(user)
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include('User Profile')
+      expect(response).to render_template(:show)
+    end
   end
 end
