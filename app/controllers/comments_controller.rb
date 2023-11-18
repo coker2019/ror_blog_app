7c9fabd53d86e9f55ca75f27c74882a128f1b370
coker2019 <olabisi2k11@gmail.com>
2023-11-18 08:26:34 +0100

    Add form to blog

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,38 @@
+class CommentsController < ApplicationController
+  def new
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    @comment.user = @user
+    if @comment.save
+      redirect_to user_post_path(@user, @post), notice: 'Comment created successfully.'
+    else
+      flash.now[:errors] = 'No comment!'
+      render :new
+    end
+  end
+
+  def destroy
+    @commentdel = Comment.includes(:post).find(params[:id])
+    @post = @commentdel.post
+
+    if @commentdel.destroy
+      flash[:notice] = 'Comment deleted!'
+    else
+      flash.now[:errors] = 'Unable to delete comment!'
+    end
+    redirect_to user_post_path(@post.user, @post)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
