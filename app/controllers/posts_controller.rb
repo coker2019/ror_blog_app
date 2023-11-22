2a2f71c20b43492b53894f045193e55c0367d3d9
coker2019 <olabisi2k11@gmail.com>
2023-11-22 19:08:56 +0100

    add test files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.includes(:author).where(author_id: params[:user_id])
-    @user = User.includes(:posts).find(params[:user_id])
+    @user = User.find(params[:user_id])
     @posts = @posts.paginate(page: params[:page], per_page: 3)
   end
 
