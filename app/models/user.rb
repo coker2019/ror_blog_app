e6686c79d40df75ca00befd4419358809aee5659
coker2019 <olabisi2k11@gmail.com>
2023-11-23 11:13:57 +0100

    update files

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
- 
+
 
   def most_recent_posts
     posts.order(created_at: :desc).limit(3)
